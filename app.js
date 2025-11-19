a8ed74411e8fff6d0e7ff5cd7b602acafeb10c6f
Sharegh Yusefi <ShareghYusefi@gmail.com>
2025-11-18 19:43:33 -0700

    data types

--- app.js
+++ app.js
@@ -28,3 +28,38 @@ console.log("redeclared y = " + y);
 var z = 10;
 // z = 20; // will error
 // let is preferred over var, and considered best practice to avoid scoping issues.
+// Data types in Typescript
+// string, number, boolean, void, null, undefined, any
+// We can define a variables data type using colon syntax
+var customMessage = "Hello World";
+var num = 10;
+var isValid = true;
+var nullValue = null;
+var undefinedVariable = undefined;
+var anyValue = 10;
+// function does not return anything have a return type of boid.
+function greet() {
+    console.log("Hello World");
+    return;
+}
+// Typeof operator
+console.log(typeof num); // number
+console.log(typeof customMessage); // number
+console.log(typeof isValid); // number
+// User-define data types
+// Arrays
+var numbers = [1, 2, 3, 4];
+var names = ["John", "Smith"];
+// Tuples: when we have an array with known number of elements and data types
+var person = ["John", 25, true];
+// Class defines a custom data type as well.
+var Person = /** @class */ (function () {
+    function Person(nameProp, ageProp, isStudentProp) {
+        this.name = nameProp;
+        this.age = ageProp;
+        this.isStudent = isStudentProp;
+    }
+    return Person;
+}());
+var person1 = new Person("John", 78, true);
+console.log(person1);
--- app.ts
+++ app.ts
@@ -37,3 +37,50 @@ const z = 10;
 // z = 20; // will error
 
 // let is preferred over var, and considered best practice to avoid scoping issues.
+
+// Data types in Typescript
+// string, number, boolean, void, null, undefined, any
+// We can define a variables data type using colon syntax
+let customMessage: string = "Hello World";
+let num: number = 10;
+let isValid: boolean = true;
+let nullValue: null = null;
+let undefinedVariable: undefined = undefined;
+let anyValue: any = 10;
+
+// function does not return anything have a return type of boid.
+function greet(): void {
+  console.log("Hello World");
+  return;
+}
+
+// Typeof operator
+console.log(typeof num); // number
+console.log(typeof customMessage); // number
+console.log(typeof isValid); // number
+
+// User-define data types
+
+// Arrays
+let numbers: number[] = [1, 2, 3, 4];
+let names: string[] = ["John", "Smith"];
+
+// Tuples: when we have an array with known number of elements and data types
+let person: [string, number, boolean] = ["John", 25, true];
+
+// Class defines a custom data type as well.
+class Person {
+  name: string;
+  age: number;
+  isStudent: boolean;
+
+  constructor(nameProp: string, ageProp: number, isStudentProp: boolean) {
+    this.name = nameProp;
+    this.age = ageProp;
+    this.isStudent = isStudentProp;
+  }
+}
+
+let person1 = new Person("John", 78, true);
+
+console.log(person1);
